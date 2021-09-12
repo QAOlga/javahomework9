01455ce3c72d6f5a01cc8e5b0d0c1edcf4bf2ed1
OlgaMikhaylyukova <olele@inbox.ru>
2021-09-12 19:21:04 +0300

    Manager added

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,23 @@
+package manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] getPostsForFeed() {
+
+        return null;
+    }
+
+    public Post[] search(ownerId, domain, query, ownersOnly, count, offset) {
+
+        return null;
+    }
+
+    public void delete(ownerId, postId) {
+
+    }
+
+
+}
--- src/main/java/ru/netology/domain/PostSourceInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.domain;
-
-public class PostSourceInfo {
-    private String type;
-    private String platform;
-    private String data;
-    private String url;
-}
