a113fd01dc817094549bece9dca71815f661fb2e
OlgaMikhaylyukova <olele@inbox.ru>
2021-09-12 14:40:18 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+public class Post {
+    private String id;
+    private String authorName;
+    private String postName;
+    private String imageUrl;
+    private String date;
+    private String time;
+    private String like;
+    private String comment;
+    private String repost;
+    private String view;
+    private String bookmark;
+
+}
