1683032642d6c8a5ea03a9dee161b905118c311e
OlgaMikhaylyukova <olele@inbox.ru>
2021-09-12 15:41:28 +0300

    Added classes

---
+++ src/main/java/ru/netology/domain/DonutInfo.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class DonutInfo {
+    private boolean isDonut;
+    private int paidDuration;
+    private PlaceHolderInfo placeHolderInfo;
+    private boolean canPublishFreeCopy;
+    private String editMode;
+}
--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,16 +1,31 @@
 package ru.netology.domain;
 
 public class Post {
-    private String id;
-    private String authorName;
-    private String postName;
-    private String imageUrl;
-    private String date;
-    private String time;
-    private String like;
-    private String comment;
-    private String repost;
-    private String view;
-    private String bookmark;
+    private int id;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private String text;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private CommentsInfo commentsInfo;
+    private CopyrightInfo copyrightInfo;
+    private LikesInfo likesInfo;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
+    private String postType;
+    private PostSourceInfo postSourceInfo;
+    private GeoInfo geoInfo;
+    private  int signerId;
+    private  int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private DonutInfo donutInfo;
+    private int postponedId;
 
 }
